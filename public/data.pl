bf0cb61ea679af569a1fa414f599dcd94cc34f1a
Jenny Schweers <orange.jenny@gmail.com>
2016-01-09 15:28:21 -0500

    limit data to past year

--- public/data.pl
+++ public/data.pl
@@ -13,23 +13,50 @@ my $dbh = Miles::DBH();
 
 print $cgi->header(-type => 'text/text');
 
-my @workouts = Miles::Results($dbh, {
-    SQL => "select id, day_id, activity, time, distance, sets, reps, weight, unit from workouts",
-    COLUMNS => [qw(id dayid activity time distance sets reps weight unit)],
+my @rows = Miles::Results($dbh, {
+    SQL => qq{
+        select
+            days.id, days.day, days.notes,
+            workouts.id, workouts.activity, workouts.time, workouts.distance, workouts.sets, workouts.reps, workouts.weight, workouts.unit
+        from
+            days, workouts
+        where
+            days.id = workouts.day_id
+            and days.day > subdate(now(), interval 1 year)
+        order by
+            days.day desc, days.id
+    },
+    COLUMNS => [qw(id day notes workoutid activity time distance sets reps weight unit)],
 });
 
-my %workoutsbydayid = map { $_->{DAYID} => [] } @workouts;
-foreach my $workout (@workouts) {
-    push(@{ $workoutsbydayid{$workout->{DAYID} }}, $workout);
+my $day = {};
+my @days = ();
+foreach my $row (@rows) {
+    if ($row->{ID} != $day->{ID}) {
+        if ($day->{ID}) {
+            push(@days, $day);
+        }
+        $day = {
+            ID => $row->{ID},
+            DAY => $row->{DAY},
+            NOTES => $row->{NOTES},
+            WORKOUTS => [],
+        };
+    }
+    my $workout = {
+        ID => $row->{WORKOUTID},
+        ACTIVITY => $row->{ACTIVITY},
+        TIME => $row->{TIME},
+        DISTANCE => $row->{DISTANCE},
+        SETS => $row->{SETS},
+        REPS => $row->{REPS},
+        WEIGHT => $row->{WEIGHT},
+        UNIT => $row->{UNIT},
+    };
+    push(@{ $day->{WORKOUTS} }, $workout);
 }
-
-my @days = Miles::Results($dbh, {
-    SQL => "select id, day, notes from days",
-    COLUMNS => [qw(id day notes)],
-});
-
-foreach my $day (@days) {
-    $day->{WORKOUTS} = $workoutsbydayid{$day->{ID}} || [];
+if ($day->{ID}) {
+    push(@days, $day);
 }
 
 print to_json(\@days);
