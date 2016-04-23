5e75ee38e92962c8fb056e50ab0b50694b1fd169
Jenny Schweers <orange.jenny@gmail.com>
2016-04-22 23:13:59 -0400

    split out legend.js

--- public/script/view/miles.js
+++ public/script/view/miles.js
@@ -5,6 +5,7 @@ requirejs([
     'underscore',
     'd3',
     'view/calendar',
+    'view/legend',
     'util/dom',
     'util/pace',
     'util/workout',
@@ -12,6 +13,7 @@ requirejs([
     _,
     d3,
     calendar,
+    legend,
     dom,
     pace,
     workout,
@@ -102,7 +104,7 @@ undefined) {
                 return day;
             });
             renderList(json);
-            renderRecords(json);
+            legend.renderLegend(json);
             calendar.renderCalendar(json);
     
             filterForm.style.display = "block";
@@ -180,129 +182,5 @@ undefined) {
         for (var i = 0; i < days.length; i++) {
             list.innerHTML += template(days[i]);
         }
-    }
-    
-    // Display legend / list of records
-    function renderRecords(allDays) {
-        var list = document.getElementById("legend"),
-            template = document.querySelector("script[type='text/template'][name='record']");
-    
-        list.innerHTML = '';
-        template = _.template(template.innerHTML);
-    
-        var daysByActivity = _.groupBy(allDays, workout.activityClass),
-            sortedActivities = _.sortBy(_.keys(daysByActivity), function(activity) {
-                return -1 * daysByActivity[activity].length;
-            });
-    
-        _.each(sortedActivities, function(activity) {
-            var records = [],
-                workouts = _.flatten(_.map(daysByActivity[activity], function(day) {    // will be sorted by day, descending
-                    return _.map(day.WORKOUTS, function(w) {
-                        return _.extend(w, {
-                            DAY: day.DAY,
-                            ID: Number(w.ID),
-                            REPS: Number(w.REPS),
-                            SETS: Number(w.SETS),
-                            WEIGHT: Number(w.WEIGHT),
-                            TIME: Number(w.TIME),
-                            DISTANCE: Number(w.DISTANCE),
-                        });
-                    });
-                }));
-            
-            // Fastest
-            if (activity === "running" || activity === "erging") {
-                var goldenDistances = [],   // lazily, but reasonably, ignoring units
-                    groupBy = '';
-                if (activity === "erging") {
-                    goldenDistances = [6, 2, 500];
-                    groupBy = 'DISTANCE';
-                } else {
-                    goldenDistances = ["long", "medium", "short"];
-                    groupBy = function(w) {
-                        if (w.DISTANCE < "5") {
-                            return "short";
-                        }
-                        if (w.DISTANCE > 10) {
-                            return "long";
-                        }
-                        return "medium";
-                    };
-                }
-                var workoutsByDistance = _.groupBy(_.filter(workouts, function(w) {
-                    return w.DISTANCE && w.TIME && (!w.REPS || w.REPS === 1);
-                }), groupBy);
-                _.each(goldenDistances, function(distance) {
-                    if (workoutsByDistance[distance]) {
-                        var min;
-                        _.each(workoutsByDistance[distance], function(w) {
-                            if (!min || min.TIME / min.DISTANCE > w.TIME / w.DISTANCE) {
-                                min = w;
-                            }
-                        });
-                        records.push({
-                            DESCRIPTION: workout.serializeWorkout(min, true),
-                            DAY: min.DAY,
-                        });
-                    }
-                });
-            }
-    
-            // Total distance covered
-            if (activity === "erging" || activity === "running") {
-                var total = 0,
-                    goldenUnit = activity === "erging" ? "km" : "mi";
-                _.each(workouts, function(w) {
-                    var distance = w.DISTANCE;
-                    if (distance) {
-                        if (w.UNIT !== goldenUnit) {
-                            distance = pace.convertDistance(w.DISTANCE, w.UNIT, goldenUnit);
-                        }
-                        total += distance;
-                    }
-                });
-                records.push({
-                    DESCRIPTION: parseInt(total) + " " + goldenUnit + " total",
-                });
-            }
-    
-            // Max weight, per lift
-            if (activity === "lifting") {
-                _.each(_.groupBy(workouts, 'ACTIVITY'), function(workouts, lift) {
-                    var max = workouts[0];
-                    _.each(workouts, function(w) {
-                        if (w.WEIGHT > max.WEIGHT) {
-                            max = w;
-                        }
-                    });
-                    records.push({
-                        DESCRIPTION: lift + "@" + max.WEIGHT,
-                        DAY: max.DAY,
-                    });
-                });
-            }
-    
-            // Total count
-            records.push({
-                DESCRIPTION: daysByActivity[activity].length + " days",
-            });
-    
-            list.innerHTML += template({
-                ACTIVITY: activity,
-                CLASS: workout.activityClass(activity),
-                RECORDS: records,
-            });
-        });
-    
-        // Add hover events to toggle record lists
-        _.each(document.querySelectorAll("#legend > li"), function(li) {
-            li.addEventListener("mouseover", function(e) {
-                e.currentTarget.querySelector(".records").style.display = "block";
-            });
-            li.addEventListener("mouseout", function(e) {
-                e.currentTarget.querySelector(".records").style.display = "none";
-            });
-        });
     }
 });
